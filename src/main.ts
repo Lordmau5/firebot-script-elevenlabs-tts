364961976d370f7471ce87393e28d31c881a2cde
Lordmau5 <mail@lordmau5.com>
2026-02-07 19:17:46 +0100

    v1.4.0

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,7 @@ const script: Firebot.CustomScript<Params> = {
 			name: 'ElevenLabs TTS',
 			description: 'A custom script that allows ElevenLabs TTS to be used in Firebot',
 			author: 'Lordmau5',
-			version: '1.3.1',
+			version: '1.4.0',
 			firebotVersion: '5'
 		};
 	},
